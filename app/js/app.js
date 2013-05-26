a9622d6727b4dbb2ba1e8204021e70bbf93e1a1a
oburakevych <oburakevych@gmail.com>
2013-05-26 11:51:18 +1000

    final changes

--- app/js/app.js
+++ app/js/app.js
@@ -15,6 +15,7 @@ edgeBudModule.BASE_URL_BACKEND = 'http://localhost\:8680/edgebud-backend';
 edgeBudModule.config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.
 	  	  when('/projects', {templateUrl: 'views/projects.html'}).
+          when('/project', {templateUrl: 'views/project.html'}).
 	  	  when('/projects/new', {templateUrl: 'views/add-project.html', controller: "ProjectController"}).
 	  	  when('/projects/:id', {templateUrl: 'views/project-details.html', controller: "ProjectDetailsController"}).
 	      otherwise({redirectTo: '/projects'});
